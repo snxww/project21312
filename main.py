b227b2202ea0e04712927e5bab346f582d36ec8a
user <itstepgoncharuk@gmail.com>
2022-12-24 10:26:24 +0200

    ABCD

---
+++ main.py
@@ -0,0 +1,19 @@
+import kem as kem
+
+result = []
+
+
+def divider(a: object, b: object) -> object:
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a / b
+
+
+data = [10, 2, 2, 5, "123", 4, 18, 0, [], 15, 8, 4]
+for key in data:
+    res = divider(key, data, kem)
+    result.append(res)
+
+print(result)
