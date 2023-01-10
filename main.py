6fa2833b9d46c4e1c4e3bb84b747bfe65464f718
user <itstepgoncharuk@gmail.com>
2023-01-10 10:33:54 +0200

    chisla

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
-import lxml
-import fake_useragent
 
-url = 'https://rozetka.com.ua/ua/duracell_5003094/p109680888/'
+def divider(a: object, b: object) -> object:
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a / b
 
-headers = {'user-agent': fake_useragent.UserAgent().random}
-#print(headers)
-respons = requests.get(url, headers=headers)
-#print(respons.status_code)
-#print(respons.text)
-
-soup= BeautifulSoup(respons.text, 'lxml')
-#(soup)
-products = soup.find_all('ul',calss_='catalog-grid ng-star-inserted')
-#print(products)
-for product in products:
-    title_product = product.find('li', class_='catalog-grid__cell catalog-grid__cell_type_slim ng-star-inserted')
-    print(title_product.text)
+result = []
+data = {10:2, 2: 5, 123: 4, 18: 0, 18: 15, 8: 4}
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except:
+        print('sorry')
+print(result)
